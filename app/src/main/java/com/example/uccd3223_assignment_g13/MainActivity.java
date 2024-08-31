c79c1415e0953be5844b2e332aeeb36845fd3f76
Lurker105 <ngyjiann@gmail.com>
2024-09-01 00:46:13 +0800

    v1.0

---
+++ app/src/main/java/com/example/uccd3223_assignment_g13/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.uccd3223_assignment_g13;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+
+public class MainActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TransactionFragment transactionFragment = new TransactionFragment();
+        HomeFragment homeFragment = new HomeFragment();
+        SettingFragment settingFragment = new SettingFragment();
+
+        BottomNavigationView BNV = findViewById(R.id.bottomNavigationView);
+        BNV.setSelectedItemId(R.id.home);
+        getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, homeFragment).commit();
+
+        BNV.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                if (item.getItemId() == R.id.transaction){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, transactionFragment).commit();
+                }
+                else if (item.getItemId() == R.id.home) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, homeFragment).commit();
+                }
+                else if (item.getItemId() == R.id.setting) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, settingFragment).commit();
+                }
+                else{
+                    return false;
+                }
+                return true;
+            }
+        });
+
+
+    }
+
+}
