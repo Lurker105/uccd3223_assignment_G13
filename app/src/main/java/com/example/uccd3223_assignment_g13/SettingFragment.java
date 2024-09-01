80d0f32e5088979ea9afb7eeab367e29d0aede71
Lurker105 <ngyjiann@gmail.com>
2024-09-01 13:48:35 +0800

    v1.1

--- app/src/main/java/com/example/uccd3223_assignment_g13/SettingFragment.java
+++ app/src/main/java/com/example/uccd3223_assignment_g13/SettingFragment.java
@@ -10,22 +10,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SettingFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class SettingFragment extends Fragment {
-
-
 
     public SettingFragment() {
         // Required empty public constructor
     }
 
-    public static SettingFragment newInstance() {
-        SettingFragment fragment = new SettingFragment();
-        return fragment;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_setting, container, false);
     }
 
     @Override
@@ -41,10 +36,4 @@ public class SettingFragment extends Fragment {
         });
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting, container, false);
-    }
 }
